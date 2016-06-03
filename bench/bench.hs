a28c42efe649f3ef488533a3720c4867e7a344f4
Patrick Pelletier <code@funwithsoftware.org>
2016-06-03 14:59:07 -0700

    port benchmark to normalization-insensitive

--- bench/bench.hs
+++ bench/bench.hs
@@ -5,7 +5,7 @@ module Main ( main) where
 
 import           Criterion.Main             ( defaultMain, bench, nf )
 import qualified Data.ByteString      as B  ( readFile )
-import qualified Data.CaseInsensitive as CI ( mk )
+import qualified Data.Unicode.NormalizationInsensitive as NI ( mk )
 import qualified NoClass              as NC ( mk )
 
 #if !MIN_VERSION_bytestring(0,10,0)
@@ -18,5 +18,5 @@ main = do
   bs <- B.readFile "pg2189.txt"
   defaultMain
     [ bench "no-class"         $ nf (\s -> NC.mk s) bs
-    , bench "case-insensitive" $ nf (\s -> CI.mk s) bs
+    , bench "normalization-insensitive" $ nf (\s -> NI.mk s) bs
     ]
